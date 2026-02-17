39648cd49cf83a8e348973e042b45e5902589690
Eric Tsai <eric492718@gmail.com>
2026-02-18 02:39:32 +0800

    Refactor ESLint configuration and dependencies for improved maintainability
    - Updated ESLint configuration files to use `defineConfig` for better type support and clarity.
    - Removed deprecated dependencies from `package.json` in the `optstuff-dashboard` app.
    - Added `@types/eslint-config-prettier` as a dependency for type definitions.
    - Streamlined the ESLint configuration for Next.js integration, enhancing compatibility and readability.

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,13 +1,14 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+// @ts-expect-error -- no types for this plugin
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
 
 /**
  * A shared ESLint configuration for the repository.
  *
- * @type {import("eslint").Linter.Config}
+ * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
   js.configs.recommended,
@@ -29,4 +30,4 @@ export const config = [
   {
     ignores: ["dist/**"],
   },
-]
+];
